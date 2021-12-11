45e9af58b0d73e1be3fc8e23121df12591197e76
Tom Gobich <tom@tomgobich.com>
2021-12-11 11:07:50 -0500

    adding comments to AuthController

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -8,18 +8,24 @@ export default class AuthController {
   }
 
   public async register({ request, response, auth }: HttpContextContract) {
+    // create validation schema for expected user form data
     const userSchema = schema.create({
       username: schema.string({ trim: true }, [rules.unique({ table: 'users', column: 'username', caseInsensitive: true })]),
       email: schema.string({ trim: true }, [rules.email(), rules.unique({ table: 'users', column: 'username', caseInsensitive: true })]),
       password: schema.string({}, [rules.minLength(8)])
     })
 
+    // get validated data by validating our userSchema
+    // if validation fails the request will be automatically redirected back to the form
     const data = await request.validate({ schema: userSchema })
 
+    // create a user record with the validated data
     const user = await User.create(data)
 
+    // login the user using the user model record
     await auth.login(user)
 
+    // redirect to the login page
     return response.redirect('/')
   }
 
@@ -28,21 +34,27 @@ export default class AuthController {
   }
 
   public async login({ request, response, auth, session }: HttpContextContract) {
+    // grab uid and password values off request body
     const { uid, password } = request.only(['uid', 'password'])
 
     try {
+      // attempt to login
       await auth.attempt(uid, password)
     } catch (error) {
+      // if login fails, return vague form message and redirect back
       session.flash('form', 'Your username, email, or password is incorrect')
       return response.redirect().back()
     }
 
+    // otherwise, redirect to home page
     return response.redirect('/')
   }
 
   public async logout({ response, auth }: HttpContextContract) {
+    // logout the user
     await auth.logout()
 
+    // redirect to login page
     return response.redirect().toRoute('auth.login.show')
   }
 }
