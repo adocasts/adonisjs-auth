6f88f5c3988c2bf8402d6d57fd1c82b1cacf8232
Tom Gobich <tom@tomgobich.com>
2021-12-12 08:42:06 -0500

    email unique validation was checking username column

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -11,7 +11,7 @@ export default class AuthController {
     // create validation schema for expected user form data
     const userSchema = schema.create({
       username: schema.string({ trim: true }, [rules.unique({ table: 'users', column: 'username', caseInsensitive: true })]),
-      email: schema.string({ trim: true }, [rules.email(), rules.unique({ table: 'users', column: 'username', caseInsensitive: true })]),
+      email: schema.string({ trim: true }, [rules.email(), rules.unique({ table: 'users', column: 'email', caseInsensitive: true })]),
       password: schema.string({}, [rules.minLength(8)])
     })
 
